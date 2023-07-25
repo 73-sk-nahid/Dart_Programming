0de88ef5ff7dd37f9e1d8abb0702dfedc867fc6b
Sk. Nahid <nahidsheikh2001@gmail.com>
2023-07-25 23:24:51 +0600

    day_01

--- Day_01/swap.dart
+++ Day_01/swap.dart
@@ -1,17 +1,19 @@
 void main() {
   int x;
   int y;
+  String X = "Value of x = ";
+  String Y = 'Value of y = ';
   x = 10;
   y = 20;
   print('Before swap');
-  print('Value of x = $x');
-  print('Value of y = $y');
+  print(X + '$x');
+  print(Y + '$y');
 
   x = x + y;
   y = x - y;
   x = x - y;
 
   print('After swap');
-  print('Value of x = $x');
-  print('Value of y = $y');
+  print(X + '$x');
+  print(Y + '$y');
 }
