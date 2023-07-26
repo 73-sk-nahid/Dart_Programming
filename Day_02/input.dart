84334a56789e0a12684e10ef981ebdfdfcf02c9a
Sk. Nahid <nahidsheikh2001@gmail.com>
2023-07-26 23:44:57 +0600

    day_02

--- Day_02/input.dart
+++ Day_02/input.dart
@@ -6,6 +6,11 @@ void main() {
   print('Enter your age: '); //23
   int? age = int.parse(stdin.readLineSync()!); //here it will take integer input
 
+  print('Introduce yourself shortly: ');
+  var data = stdin.readLineSync(); //it will take any type of input
+
   print(
       "your name is $name and your age is $age"); //your name is Sk. Nahid and your age is 23
+
+  print('About yourself: $data');
 }
