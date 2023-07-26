2f2d60c7052b416ebb40388b077447a93038ec2c
Sk. Nahid <nahidsheikh2001@gmail.com>
2023-07-26 23:57:06 +0600

    day_02

---
+++ Day_02/celsius_to_fahrenheit.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter temperature(Celsius):");
+  double celsius = double.parse(stdin.readLineSync()!);
+
+  double fahrenheit = celsius * 9 / 5 + 32;
+
+  stdout.write('The temperature in fahrenheit is $fahrenheit');
+}
