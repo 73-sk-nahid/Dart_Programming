7d59eda5fe4d7aff199a211e75a46c229adf59af
Sk. Nahid <nahidsheikh2001@gmail.com>
2023-07-27 18:05:07 +0600

    day_03

---
+++ Day_03/if_condition.dart
@@ -0,0 +1,13 @@
+void main() {
+  int x = 20;
+  int y = 10;
+
+  if (x > y) {
+    print('x is greater then y');
+  }
+
+  if (!(x > y)) {
+    //it will not enter this condition because of not logical operator
+    print('x is greater then y');
+  }
+}
