c93e1db8fe5cabdbbb814ccf17001ab04ef57c58
Sk. Nahid <nahidsheikh2001@gmail.com>
2023-07-29 05:03:08 +0600

    day_04

---
+++ Day_04/list.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> name = ['Sk. Nahid', 'Rahim', 'Karim', 'Kamal', 'Jamal'];
+  List<int> age = [23, 20, 24, 25, 22];
+
+  for (int i = 0; i < name.length; i++) {
+    print('${name[i]} is a person and his age is ${age[i]}');
+  }
+}
