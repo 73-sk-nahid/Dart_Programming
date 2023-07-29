6dc4b58d11df366c85a2afaf42d61e310b5f4a92
Sk. Nahid <nahidsheikh2001@gmail.com>
2023-07-29 21:19:53 +0600

    day_05

--- Day_05/map.dart
+++ Day_05/map.dart
@@ -1,13 +1,12 @@
 void main() {
-  Map<String, String> map = {
-    "Name": "Sk. Nahid"
-  }; //for String, String you can't enter any other types value
-  Map<String, int> map1 = {
-    "Age": 22
-  }; //for String, int you have to put a String and an integer value
-  Map<String, dynamic> map3 = {
-    "Address": "258 M.A. Sattar Road"
-  }; /*by using of dynamic instead of String or integer 
+  //for String, String you can't enter any other types value
+  Map<String, String> map = {"Name": "Sk. Nahid"};
+  //for String, int you have to put a String and an integer value
+  Map<String, int> map1 = {"Age": 22};
+  /*by using of dynamic instead of String or integer 
   its gives permission to use any type of data. 
   It can be String or integer*/
+  Map<String, dynamic> map2 = {"Address": "258 M.A. Sattar Road"};
+
+  print('All the values are $map , $map1 and $map2');
 }
