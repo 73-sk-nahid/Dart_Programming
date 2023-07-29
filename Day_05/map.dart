58106b876ba5aec1897d2c13a1e25bbe7f43a63f
Sk. Nahid <nahidsheikh2001@gmail.com>
2023-07-29 21:16:12 +0600

    day_05

---
+++ Day_05/map.dart
@@ -0,0 +1,13 @@
+void main() {
+  Map<String, String> map = {
+    "Name": "Sk. Nahid"
+  }; //for String, String you can't enter any other types value
+  Map<String, int> map1 = {
+    "Age": 22
+  }; //for String, int you have to put a String and an integer value
+  Map<String, dynamic> map3 = {
+    "Address": "258 M.A. Sattar Road"
+  }; /*by using of dynamic instead of String or integer 
+  its gives permission to use any type of data. 
+  It can be String or integer*/
+}
